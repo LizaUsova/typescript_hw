66c8dba96b37d188ba60cf52369f68a788a7035b
Liza Usova <Liza.usova@gmail.com>
2024-10-26 11:09:19 +0300

    hw_5 completed

--- src/file.js
+++ src/file.js
@@ -1,25 +1,20 @@
-function deepClone<T extends Record<string, any> | any[]>(obj: T): T {
+"use strict";
+function deepClone(obj) {
     if (obj === null || typeof obj !== 'object') {
         return obj;
     }
-
-    const clonedObject = (Array.isArray(obj) ? [] : {}) as T;
-
+    const clonedObject = (Array.isArray(obj) ? [] : {});
     Object.keys(obj).forEach(key => {
-        (clonedObject as any)[key] = deepClone((obj as any)[key]);
+        clonedObject[key] = deepClone(obj[key]);
     });
-
     return clonedObject;
 }
-
 const original = {
     a: 1,
     b: { c: 2 },
     d: [3, 4]
 };
-
 const copy = deepClone(original);
-
 console.log(copy);
 console.log(original !== copy);
 console.log(original.b !== copy.b);
