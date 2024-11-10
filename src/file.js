8879d8a5507b4ff98a3a2d0e5cbfd87543229d43
Liza Usova <Liza.usova@gmail.com>
2024-11-10 14:52:17 +0200

    hw_10 completed

--- src/file.js
+++ src/file.js
@@ -1,19 +1,11 @@
-type User = {
-    id: number;
-    name: string;
-    age: number;
-    isActive: boolean;
-};
-
-const users: User[] = [
+"use strict";
+const users = [
     { id: 1, name: 'Alice', age: 25, isActive: true },
     { id: 2, name: 'Bob', age: 30, isActive: false },
     { id: 3, name: 'Charlie', age: 35, isActive: true },
 ];
-
-function filterByProperty<T, K extends keyof T>(array: T[], property: K, value: T[K]): T[] {
+function filterByProperty(array, property, value) {
     return array.filter(item => item[property] === value);
 }
-
 const activeUsers = filterByProperty(users, 'isActive', true);
 console.log(activeUsers);
