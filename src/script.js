bf9d2ef9d71d41e45f8d4ab274e5474de48d9e2b
Liza Usova <Liza.usova@gmail.com>
2024-11-25 02:15:42 +0200

    hw_13 step_2 completed

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@
 class User {
     static nextId = 1;
     #password;
-    constructor(id, name, email, password) {
+    constructor(name, email, password) {
         this.id = User.nextId++;
         this.name = name;
         this.email = email;
@@ -16,5 +16,35 @@ class User {
 
     get info() {
         return `ID: [${this.id}], Name: [${this.name}], Email: [${this.email}]`
+    }
+}
+
+class Student extends User {
+    constructor(name, email, password) {
+        super(name, email, password);
+        this.courses = [];
+    }
+
+    enroll(course) {
+    if (!this.courses.includes(course)) {
+            this.courses.push(course)
+        } else {
+            return console.log(`this course ${course} already exists`)
+        }
+    }
+}
+
+class Teacher extends User {
+constructor(name, email, password) {
+    super(name, email, password);
+    this.subjects = [];
+}
+
+    addSubject(subject) {
+        if (!this.subjects.includes(subject)) {
+            this.subjects.push(subject)
+        } else {
+            return console.log(`this subject ${subject} already exists`)
+        }
     }
 }
